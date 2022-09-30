688ddfe66ace198126a3d3fb954d651e71e5b55a
saidurgaprasad yalla <la1f@allegiantair.com>
2022-09-30 16:31:29 +0530

    updated the php file

--- src/index.php
+++ src/index.php
@@ -20,29 +20,30 @@ class MyClass
 	/* Setter function to change the attribute */
 	public function set_name($new_name)
 	{
-		if ($this->is_valid_name($new_name))
-		{
-			$this->name = $new_name;
-		}
+		// if ($this->is_valid_name($new_name))
+		// {
+		// 	$this->name = $new_name;
+		// }
+		$this->name = $new_name;
 	}
 	
 	/* Checks if the name is valid */
-	private function is_valid_name($name)
-	{
-		$valid = TRUE;
+	// private function is_valid_name($name)
+	// {
+	// 	$valid = TRUE;
 		
-		/* Just checks if the string length is between 3 and 16 */
-		if (strlen($name) < 3)
-		{
-			$valid = FALSE;
-		}
-		else if (strlen($name) > 16)
-		{
-			$valid = FALSE;
-		}
+	// 	/* Just checks if the string length is between 3 and 16 */
+	// 	if (strlen($name) < 3)
+	// 	{
+	// 		$valid = FALSE;
+	// 	}
+	// 	else if (strlen($name) > 16)
+	// 	{
+	// 		$valid = FALSE;
+	// 	}
 		
-		return $valid;
-	}
+	// 	return $valid;
+	// }
 }
 
 $mc = new MyClass();
